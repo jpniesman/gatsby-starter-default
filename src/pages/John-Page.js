4150f78bd315ec979e8cb4c923ab6ae24fb51c73
John Niesman <john-niesman@uiowa.edu>
2022-11-29 16:03:29 -0600

    team page done

--- src/pages/John-Page.js
+++ src/pages/John-Page.js
@@ -10,15 +10,36 @@ const PostTemplate = () => (
     <Layout>
         <h1>Meet John Niesman...</h1>
         <StaticImage
-            src="../images/john_group.png"
+            src="../images/john_headshot.jpg"
             loading="eager"
-            // width={64}
-            // quality={95}
+            height={600}
             formats={["auto", "webp", "avif"]}
             alt=""
             style={{ marginBottom: `var(--space-3)` }}
         />
-        <p>Leave a comment below</p>
+        <h2>About Me</h2>
+            <p>Hello my name is John Niesman. I am a CSE student at Iowa. I will be graduating in May of 2023 and going on to work at CNH Industrial. I interned with CNH last summer as a software test engineer and created a new data tool for them to use. I plan on living in the Chicago area starting next summer. </p>
+        <StaticImage
+            src="../images/john_family.png"
+            loading="eager"
+            formats={["auto", "webp", "avif"]}
+            alt=""
+            style={{ marginBottom: `var(--space-3)` }}
+        />
+        <h2>Other Interests</h2>
+             <p>Besides school, I have many other things going on in my life. During the winter I do a lot of skiing. My favorite places to ski are Jackson Hole Wy and Breckeridge CO. I spend a lot of time with friedns goingto concerts, sports games, and just hanging out. I am a huge Chicago sports fan and love going to all games especially the Cubs. Lastly, I spend a lot of time outdoors doing thigs like hiking, fishing, and camping.</p>
+        <StaticImage
+            src="../images/john_group.png"
+            loading="eager"
+            formats={["auto", "webp", "avif"]}
+            alt=""
+            style={{ marginBottom: `var(--space-3)` }}
+        />
+        <h2>Contact Information</h2>
+            <p>Email: jpniesman@gmail.com</p>
+            <p>Phone Number: 847-363-7903</p>
+
+        <h3>Leave a comment below</h3>
         <div><Comments/></div>
         <Link to="/">Go back to the homepage</Link>
     </Layout>
--- src/pages/team-intro.js
+++ src/pages/team-intro.js
@@ -2,71 +2,56 @@ import * as React from "react"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import Comments from '../components/Comments'
 import {Link} from "gatsby";
+import {StaticImage} from "gatsby-plugin-image";
 
 const PostTemplate = () => (
-  <Layout>
-    <h1>Hi from the comments page</h1>
-    <p>Leave a comment below</p>
-    <div><Comments/></div>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
+    <Layout>
+        <h1>Meet The Team 6 Group Members...</h1>
+        <StaticImage
+            src="../images/team_pic_brothers.jpg"
+            loading="eager"
+            // height={600}
+            formats={["auto", "webp", "avif"]}
+            alt=""
+            style={{ marginBottom: `var(--space-3)` }}
+        />
+        <h2>Zach Hogan</h2>
+        <p>Hello my name is Zach Hogan. I am a CSE at Iowa with a focus area in Software Engineering. I am proficient in Java, C#, and Python. I have experience writing tests for my code and working with test-driven-development in mind. I have developed using MVC in class (using Ruby on Rails) and in a prior internship (using Java Springboot). I also have experience in an agile work environment.
+                I am interested in working in teams, helping other people, and continuing to learn more about the tech industry and some of the cool things I can create by writing code.</p>
+        <h3>Zach's Contact Info</h3>
+            <p>Email:zhogan137@gmail.com</p>
+            <p>Phone Number: 847-840-8939</p>
+
+        <h2>Brett Dresbach </h2>
+        <p>Hello my name is Brett Dresbach. I am an EE at Iowa with a focus area in Power Systems. I am proficient at circuit design,  analysis, and  building. I also have experience writing MATLAB scripts and object oriented programming. Some projects that I have worked on include IR receiver and transmitter, receptacle panel replacement, and substation design.
+                Teams are a great form of collaberation, which is why I love them. Every project goes a lot smoother with someone else to bounce ideas off of. </p>
+        <h3>Brett's Contact Info</h3>
+            <p>Email:bdres34@yahoo.com</p>
+            <p>Phone Number: 815-822-3892</p>
+
+        <h2>John Niesman</h2>
+        <p>Hello, my name is John Niesman. I am a
+                senior CSE student at Iowa. My focus area
+                is in Software Enginnering. I have
+                extensive progrmaming expereicne with
+                Pyhton, Java, and C++.  I have built
+                applications using the Flask framework
+                as well as Ruby on Rails. I have developed
+                these apps using TDD principles.
+
+                In school as well as in my own life, I enjoy
+                working with others and learning new
+                things. I love trying to things with code
+                and seeing what I am able to do with
+                modern languages. </p>
+        <h3>John's Contact Info</h3>
+                <p>Email: jpniesman@gmail.com</p>
+                <p>Phone Number: 847-363-7903</p>
+
+        <Link to="/">Go back to the homepage</Link>
+    </Layout>
 )
 
 export const Head = () => <Seo title="Page two" />
 export default PostTemplate
-
-// import {graphql, Link} from "gatsby"
-// export const Head = ({ data: { markdownRemark: post } }) => {
-//     return (
-//         <Seo
-//             title={post.frontmatter.title}
-//             description={post.frontmatter.description || post.excerpt}
-//         />
-//     )
-// }
-
-// export default PostTemplate
-// export const pageQuery = graphql`
-//   query BlogPostBySlug(
-//     $id: String!
-//     $previousPostId: String
-//     $nextPostId: String
-//   ) {
-//     site {
-//       siteMetadata {
-//         title
-//       }
-//     }
-//     markdownRemark(id: { eq: $id }) {
-//       id
-//       excerpt(pruneLength: 160)
-//       html
-//       fields {
-//         slug
-//       }
-//       frontmatter {
-//         title
-//         date(formatString: "MMMM DD, YYYY")
-//         description
-//       }
-//     }
-//     previous: markdownRemark(id: { eq: $previousPostId }) {
-//       fields {
-//         slug
-//       }
-//       frontmatter {
-//         title
-//       }
-//     }
-//     next: markdownRemark(id: { eq: $nextPostId }) {
-//       fields {
-//         slug
-//       }
-//       frontmatter {
-//         title
-//       }
-//     }
-//   }
-// `
